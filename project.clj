fad15fd476a1d4ffa7e9eeb057989f30b99f13fc
Christian Müller <chmllr@me.com>
2014-10-08 21:46:12 +0200

    project integrated into Cursive

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject wikizen "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
+  :description "Simple Markdown-based Wiki engine"
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2311"]]
 
-  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]
+            [lein-simpleton "1.3.0"]]
 
   :source-paths ["src"]
 
