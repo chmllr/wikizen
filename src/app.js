cdfceb4c7e748ee9f531e6bee0cb754f36921b03
Christian Müller <chmllr@me.com>
2015-01-18 22:06:36 +0100

    simple hash based router implemented

--- src/app.js
+++ src/app.js
@@ -3,11 +3,19 @@
 var React = require('react');
 //var EventBus = require('eventbus');
 var engine = require('./engine');
+var router = require('./router');
 
 var defaultRootPage = engine.createPage("Main Page", "Hello world!");
 var wiki = engine.createWiki("Test Wiki", defaultRootPage);
 var runtimeArtifact = engine.assembleRuntimeWiki(wiki);
 var container = document.getElementById("app");
+
+var Link = React.createClass({
+    render: function () {
+        var props = this.props;
+        return <a href={"#" + props.to}>{props.label}</a>
+    }
+});
 
 var Page = React.createClass({
     render: function () {
@@ -17,9 +25,17 @@ var Page = React.createClass({
             <div>{page.body}</div>
             <hr/>
             <h2>{page.children.length == 0 ? null : "Nested Pages"}</h2>
-            <ul>{page.children.map(child => <li>{child.title}</li>)}</ul>
+            <ul>{page.children.map(child => <li><Link to={"page=" + child.id}
+                label={child.title} /></li>)}</ul>
         </div>
     }
 });
 
-React.render(<Page page={runtimeArtifact.root} />, container);
+router.addHandler("page=:id", params =>
+    React.render(<Page page={runtimeArtifact.index.ids[params.id]} />, container));
+
+router.addHandler("edit=:id", params => console.log("page editor", params));
+router.addHandler("delete=:id", params => console.log("page deleter", params));
+
+self.onhashchange = router.dispatcher;
+location.href = "#page=0";
---
+++ src/router.js
@@ -0,0 +1,27 @@
+function Router () {
+    var handlers = {};
+    this.addHandler = (pattern, handler) => {
+        var list = handlers[pattern] || [];
+        list.push(handler);
+        handlers[pattern] = list;
+    };
+
+    this.dispatcher = () =>
+        Object.keys(handlers)
+            .map(pattern => ({
+                identifiers: pattern.match(/:\w+/g).map(x => x.replace(":", "")),
+                matches: location.hash
+                    .replace("#", "")
+                    .match(new RegExp(pattern.replace(/:\w+/g, "(.+)"))),
+                handlers: handlers[pattern]
+            }))
+            .filter(object => object.matches)
+            .forEach(object =>
+                object.handlers.forEach(handler =>
+                    handler(object.identifiers.reduce((acc, param, i) => {
+                            acc[param] = object.matches[i + 1];
+                            return acc
+                        }, {}))));
+}
+
+module.exports = new Router();
