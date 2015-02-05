b85e83a3f552116ccae6e0c081ebee9a5bb5f55f
Christian Müller <chmllr@me.com>
2015-02-05 12:02:45 +0100

    UI refactoring

--- src/app.js
+++ src/app.js
@@ -1,265 +1,33 @@
 "use strict";
 
-var React = require('react');
 var State = require('./state');
 var Router = require('./router');
-var marked = require('marked');
+var UI = require('./ui');
 
 var wikiZenURL = location.protocol + "//" + location.host + location.pathname;
-
-var runtime;
-var renderComponent = component => React.render(component, document.body);
-var openPage = id => location.hash = "#page=" + id;
-var editPage = id => location.hash = "#edit=" + id;
-var addPage = id => location.hash = "#add=" + id;
-var deletePage = id => location.hash = "#delete=" + id;
 self.onhashchange = Router.dispatcher;
-var keyMapping = {
-    69: "edit",
-    65: "add",
-    68: "delete",
-    37: "back",
-    27: "escape",
-    48: "home",
-    83: "save",
-    49: 1,
-    50: 2,
-    51: 3,
-    52: 4,
-    53: 5,
-    54: 6,
-    55: 7,
-    56: 8,
-    57: 9
-};
+var appState;
 
-var Link = React.createClass({
-    render: function () {
-        var props = this.props;
-        var param = props.param;
-        return <a className={props.className}
-            href={"#" + props.to + (param != undefined ? "=" + param : "")}>{props.label}</a>
-    }
-});
-
-var Breadcrumb = React.createClass({
-    getPath: function (pageID) {
-        var parent = runtime.getParent(pageID);
-        if(!parent) return [];
-        var node = <Link key={parent.id} to="page" param={parent.id} label={parent.title} />;
-        var rest = this.getPath(parent.id);
-        rest.push(node);
-        rest.push(<span key={"span" + rest.length}>&nbsp;&gt;&nbsp;</span>);
-        return rest;
-    },
-    render: function () {
-        var props = this.props;
-        var path = this.getPath(props.id);
-        path.push(props.title);
-        return <nav className="Breadcrumb">{path}</nav>;
-    }
-});
-
-var NestedPages = React.createClass({
-    render: function () {
-        var pages = this.props.pages;
-        return pages.length == 0 ? null : <div><b>Nested Pages</b>
-            <ol>{pages.map((page, i) =>
-                <li key={i}><Link to="page" param={page.id} label={page.title} /></li>)}
-            </ol>
-        </div>;
-    }
-});
-
-var Sidebar = React.createClass({
-    getInitialState: function () {
-        return { menuHidden: true }
-    },
-    render: function () {
-        var page = this.props;
-        var id = page.id;
-        var children = page.children;
-        var isRoot = id == 0;
-        return <aside>
-            <button className="BackButton"
-                disabled={isRoot}
-                onClick={() => openPage(runtime.getParent(id).id)}>
-                <span className="monospace">&lt; </span>Back</button>
-            <div className="separator"></div>
-            <button className="prime" onClick={() => addPage(id)}>Add Page</button>
-            <button onClick={() => editPage(id)}>Edit Page</button>
-            <button onClick={() => this.setState({ menuHidden: !this.state.menuHidden })}>
-                <span className="monospace">{this.state.menuHidden ? "+" : "-"} </span>
-                Menu</button>
-            { this.state.menuHidden
-                ? <div className="separator"></div>
-                : <ul className="Menu">
-                {isRoot ? null : <li><Link to="delete" param={id} label="Delete Page" /></li>}
-                <li><Link to="export" label="Export Wiki" /></li>
-                <li><Link to="signout" label="Sign Out" /></li>
-            </ul>}
-            {children.length == 0 || !page.body ? null : <NestedPages pages={page.children} />}
-            <div className="filler"></div>
-            <footer>Powered by <Link to="landing" label="WikiZen"/></footer>
-        </aside>
-    }
-});
-
-var Page = React.createClass({
-    componentDidMount: function () {
-        document.onkeydown = event => {
-            var page = this.props;
-            var code = keyMapping[event.keyCode];
-            switch (code) {
-                case "home":
-                    openPage(0);
-                    break;
-                case "edit":
-                    editPage(page.id);
-                    break;
-                case "add":
-                    addPage(page.id);
-                    break;
-                case "delete":
-                    deletePage(page.id);
-                    break;
-                case "back":
-                    var parent = runtime.getParent(page.id);
-                    if(parent) openPage(parent.id);
-                    break;
-                default:
-                    var child = page.children[code-1];
-                    if(child) openPage(child.id);
-            }
-        };
-    },
-    render: function () {
-        var page = this.props;
-        var body = page.body;
-        return <div className="Page">
-            <Sidebar {...page}/>
-            <main>
-                <Breadcrumb {...page} />
-                {body
-                    ? <article key={0} dangerouslySetInnerHTML={{__html: marked(body || "") }}></article>
-                    : <article key={1}><h1>{page.title}</h1><NestedPages pages={page.children} /></article>}
-            </main>
-        </div>
-    }
-});
-
-var EditingForm = React.createClass({
-    getInitialState: function () {
-        var props = this.props;
-        return props.mode == "EDIT" ? runtime.getPage(props.pageID) : {};
-    },
-    applyChanges: function () {
-        var pageID;
-        var props = this.props;
-        if (props.mode == "ADD")
-            pageID = runtime.addPage(props.pageID,
-                this.refs.title.getDOMNode().value,
-                this.refs.body.getDOMNode().value);
-        else {
-            pageID = props.pageID;
-            var state = this.state;
-            runtime.editPage(pageID, state.title, state.body);
-        }
-        openPage(pageID);
-    },
-    handleChange: function (property, value) {
-        var state = {};
-        state[property] = value;
-        this.setState(state);
-    },
-    componentDidMount: function () {
-        this.refs.title.getDOMNode().focus();
-        document.onkeydown = event => {
-            var props = this.props;
-            var id = props.pageID;
-            var code = keyMapping[event.keyCode];
-            switch (code) {
-                case "escape":
-                    var page = props.mode == "EDIT" ? runtime.getPage(id) : runtime.getParent(id);
-                    openPage(page && page.id || 0);
-                    break;
-                case "save":
-                    if (event.metaKey || event.ctrlKey && !event.altKey) {
-                        event.preventDefault();
-                        this.applyChanges();
-                    }
-                    break;
-                default:
-                    break;
-            }
-        };
-    },
-    render: function () {
-        var state = this.state;
-        return <div className="EditingForm">
-            <input className="TitleInput" ref="title" type="text" placeholder="Title" value={state.title}
-                onChange={event => this.handleChange("title", event.target.value)}/>
-            <textarea className="BodyInput" ref="body" value={state.body}
-                onChange={event => this.handleChange("body", event.target.value)}></textarea>
-            <main className="Scrollable">
-                <article className="Main" dangerouslySetInnerHTML={{__html: marked(state.body || "")}}></article>
-            </main>
-            <div className="ButtonBar">
-                <button onClick={() => window.history.back()}>Cancel</button>
-                <button onClick={this.applyChanges}>
-                {this.props.mode == "EDIT" ? "Save Page" : "Create New Page"}
-                </button>
-            </div>
-        </div>
-    }
-});
-
-var LandingPage = React.createClass({
-    render: function () {
-        return <div id="LandingPage" className="CenteredBox">
-            <div className="Logo">
-                <img src="images/wikizen.svg" />
-                WikiZen
-            </div>
-            <p className="Lead">Simple Markdown Wiki in your Dropbox.</p>
-            <div className="ButtonBar">
-                <button onClick={() => signIn()}>Connect With Dropbox</button>
-            &nbsp;
-                <button onClick={() => signIn('local')}
-                    className="prime">Give It 5 Minutes</button>
-            </div>
-            <a href="https://github.com/chmllr/WikiZen">
-                <img style={{position: "absolute", top: 0, right: 0, border: 0}}
-                    src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
-                    alt="Fork me on GitHub" />
-            </a>
-        </div>;
-    }
-});
+var openPage = id => location.hash = "#page=" + id;
 
 Router.addHandler("page=:id", params => {
     var id = params.id;
-    var page = runtime.getPage(id);
-    localStorage.setItem(runtime.getProvider() + ".openedPage", id);
-    renderComponent( page
-        ? <Page {...page} />
-        : <div className="CenteredBox">Unknown page ID.</div> )
+    var page = appState.getPage(id);
+    localStorage.setItem(appState.getProvider() + ".openedPage", id);
+    if(page) UI.render.PAGE(page);
+    else UI.render.MESSAGE("Unknown page ID.");
 });
 
-Router.addHandler("add=:id", params =>
-    renderComponent(<EditingForm mode="ADD" pageID={params.id} />));
+Router.addHandler("add=:id", params => UI.render.EDIT_FORM({ mode: "ADD", pageID: params.id }));
 
-Router.addHandler("edit=:id", params =>
-    renderComponent(<EditingForm mode="EDIT" pageID={params.id} />));
+Router.addHandler("edit=:id", params => UI.render.EDIT_FORM({ mode: "EDIT", pageID: params.id }));
 
-Router.addHandler("export", () =>
-    renderComponent(<textarea className="Export">{JSON.stringify(runtime.getPage(0), null, 2)}</textarea>));
+Router.addHandler("export", () => UI.render.EXPORT_PAGE(JSON.stringify(appState.getPage(0), null, 2)));
 
-Router.addHandler("landing", () =>
-    renderComponent(<LandingPage />));
+Router.addHandler("landing", () => UI.render.LANDING_PAGE());
 
 Router.addHandler("signout", () => {
-    runtime.signOut();
+    appState.signOut();
     localStorage.clear();
     location.href = wikiZenURL;
 });
@@ -267,9 +35,9 @@ Router.addHandler("signout", () => {
 Router.addHandler("delete=:id", params => {
     var response = confirm("Are you sure?");
     if (response) {
-        var parent = runtime.getParent(params.id);
+        var parent = appState.getParent(params.id);
         if(parent) {
-            runtime.deletePage(params.id);
+            appState.deletePage(params.id);
             openPage(parent.id);
         }
     }
@@ -277,11 +45,12 @@ Router.addHandler("delete=:id", params => {
 
 self.signIn = provider => {
     if(!provider != "local") localStorage.loggedIn = true;
-    runtime = new State(provider);
-    renderComponent(<div className="CenteredBox">Loading...</div>);
-    runtime.init().then(() => openPage(localStorage.getItem(runtime.getProvider() + ".openedPage") || 0),
+    appState = new State(provider);
+    UI.setAppState(appState);
+    UI.render.MESSAGE("Connecting...");
+    appState.init().then(() => openPage(localStorage.getItem(appState.getProvider() + ".openedPage") || 0),
         console.error);
 };
 
-if (location.hash == "#landing" || !localStorage.loggedIn) renderComponent(<LandingPage />);
+if (location.hash == "#landing" || !localStorage.loggedIn) UI.render.LANDING_PAGE();
 else self.signIn();
